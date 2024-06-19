d921c93aaef3966715f04c1f9bf73e9b2df6deb3
Abel Gomez Mendez <abel.gomez@corp.etecsa.cu>
2024-06-19 15:22:19 -0400

    Commit con programa funcional, a falta de mejoras visuales

--- procederes.py
+++ procederes.py
@@ -61,5 +61,223 @@ def actualiza_estado_de_cuenta_eCRM(db):
         return None
     return conteo
 
-problemas_cola(db)
-actualiza_estado_de_cuenta_eCRM(db)
+
+def cierra_gestiones_de_cobro_eCRM(db):
+    """
+    Actualiza el valor del campo 'get_service_to_call_data_from_sgc' a 'false' en todos los documentos de la colección 'trace_process'
+    donde el campo 'get_service_to_call_data_from_sgc' contiene el valor 'true', independientemente de su formato (mayúsculas/minúsculas).
+
+    Args:
+        db (MongoClient): El cliente de MongoDB conectado al servidor.
+
+    Returns:
+        int: El número de documentos actualizados. Retorna 0 si no se encontraron documentos que coincidan con el criterio de búsqueda.
+    """
+    try:
+        actualizacion = db.get_collection('trace_process').update_many(
+            {"get_service_to_call_data_from_sgc": {"$regex": "\"true\"", "$options" : "i"}},  # Criterio de búsqueda
+            {"$set": {"get_service_to_call_data_from_sgc": "false"}}  # Operación de actualización
+        )
+        conteo = 0
+        if actualizacion.modified_count != None:
+            conteo = actualizacion.modified_count
+        print(f'La cantidad de documentos encontrados y con el campo get_service_to_call_data_from_sgc puestos en false es: {conteo}')
+        
+
+    except Exception as e:
+        print(f'Error al procesar la consulta: {e}')
+        return None
+    return conteo
+
+
+def export_fichero_Transfermovil_eCRM(db):
+    """
+    Actualiza el valor del campo 'export_tranfermovil_operation' a 'false' en todos los documentos de la colección 'trace_process'
+    donde el campo 'export_tranfermovil_operation' contiene el valor 'true', independientemente de su formato (mayúsculas/minúsculas).
+
+    Args:
+        db (MongoClient): El cliente de MongoDB conectado al servidor.
+
+    Returns:
+        int: El número de documentos actualizados. Retorna 0 si no se encontraron documentos que coincidan con el criterio de búsqueda.
+    """
+    try:
+        actualizacion = db.get_collection('trace_process').update_many(
+            {"export_tranfermovil_operation": {"$regex": "\"true\"", "$options" : "i"}},  # Criterio de búsqueda
+            {"$set": {"export_tranfermovil_operation": "false"}}  # Operación de actualización
+        )
+        conteo = 0
+        if actualizacion.modified_count != None:
+            conteo = actualizacion.modified_count
+        print(f'La cantidad de documentos encontrados y con el campo export_tranfermovil_operation puestos en false es: {conteo}')
+        
+
+    except Exception as e:
+        print(f'Error al procesar la consulta: {e}')
+        return None
+    return conteo
+
+
+def descarga_fichero_banco_eCRM(db):
+    """
+    Actualiza el valor del campo 'insert_in_mongo_ftp_files' a 'false' en todos los documentos de la colección 'trace_process'
+    donde el campo 'insert_in_mongo_ftp_files' contiene el valor 'true', independientemente de su formato (mayúsculas/minúsculas).
+
+    Args:
+        db (MongoClient): El cliente de MongoDB conectado al servidor.
+
+    Returns:
+        int: El número de documentos actualizados. Retorna 0 si no se encontraron documentos que coincidan con el criterio de búsqueda.
+    """
+    try:
+        actualizacion = db.get_collection('trace_process').update_many(
+            {"insert_in_mongo_ftp_files": {"$regex": "\"true\"", "$options" : "i"}},  # Criterio de búsqueda
+            {"$set": {"insert_in_mongo_ftp_files": "false"}}  # Operación de actualización
+        )
+        conteo = 0
+        if actualizacion.modified_count != None:
+            conteo = actualizacion.modified_count
+        print(f'La cantidad de documentos encontrados y con el campo insert_in_mongo_ftp_files puestos en false es: {conteo}')
+        
+
+    except Exception as e:
+        print(f'Error al procesar la consulta: {e}')
+        return None
+    return conteo
+
+
+def crea_tablas_old_debt_comercial_eCRM(db):
+    """
+    Actualiza el valor del campo 'created_old_debt' a 'false' en todos los documentos de la colección 'trace_process'
+    donde el campo 'created_old_debt' contiene el valor 'true', independientemente de su formato (mayúsculas/minúsculas).
+
+    Args:
+        db (MongoClient): El cliente de MongoDB conectado al servidor.
+
+    Returns:
+        int: El número de documentos actualizados. Retorna 0 si no se encontraron documentos que coincidan con el criterio de búsqueda.
+    """
+    try:
+        actualizacion = db.get_collection('trace_process').update_many(
+            {"created_old_debt": {"$regex": "\"true\"", "$options" : "i"}},  # Criterio de búsqueda
+            {"$set": {"created_old_debt": "false"}}  # Operación de actualización
+        )
+        conteo = 0
+        if actualizacion.modified_count != None:
+            conteo = actualizacion.modified_count
+        print(f'La cantidad de documentos encontrados y con el campo created_old_debt puestos en false es: {conteo}')
+        
+
+    except Exception as e:
+        print(f'Error al procesar la consulta: {e}')
+        return None
+    return conteo
+
+
+def concilia_cobros_transfermovil_eCRM(db):
+    """
+    Actualiza el valor del campo 'daily_conciliation_transfer_payments' a 'false' en todos los documentos de la colección 'trace_process'
+    donde el campo 'daily_conciliation_transfer_payments' contiene el valor 'true', independientemente de su formato (mayúsculas/minúsculas).
+
+    Args:
+        db (MongoClient): El cliente de MongoDB conectado al servidor.
+
+    Returns:
+        int: El número de documentos actualizados. Retorna 0 si no se encontraron documentos que coincidan con el criterio de búsqueda.
+    """
+    try:
+        actualizacion = db.get_collection('trace_process').update_many(
+            {"daily_conciliation_transfer_payments": {"$regex": "\"true\"", "$options" : "i"}},  # Criterio de búsqueda
+            {"$set": {"daily_conciliation_transfer_payments": "false"}}  # Operación de actualización
+        )
+        conteo = 0
+        if actualizacion.modified_count != None:
+            conteo = actualizacion.modified_count
+        print(f'La cantidad de documentos encontrados y con el campo daily_conciliation_transfer_payments puestos en false es: {conteo}')
+        
+
+    except Exception as e:
+        print(f'Error al procesar la consulta: {e}')
+        return None
+    return conteo
+
+
+def concilia_cobros_semanales_transfermovil_eCRM(db):
+    """
+    Actualiza el valor del campo 'weekly_conciliation_transfer_payments' a 'false' en todos los documentos de la colección 'trace_process'
+    donde el campo 'weekly_conciliation_transfer_payments' contiene el valor 'true', independientemente de su formato (mayúsculas/minúsculas).
+
+    Args:
+        db (MongoClient): El cliente de MongoDB conectado al servidor.
+
+    Returns:
+        int: El número de documentos actualizados. Retorna 0 si no se encontraron documentos que coincidan con el criterio de búsqueda.
+    """
+    try:
+        actualizacion = db.get_collection('trace_process').update_many(
+            {"weekly_conciliation_transfer_payments": {"$regex": "\"true\"", "$options" : "i"}},  # Criterio de búsqueda
+            {"$set": {"weekly_conciliation_transfer_payments": "false"}}  # Operación de actualización
+        )
+        conteo = 0
+        if actualizacion.modified_count != None:
+            conteo = actualizacion.modified_count
+        print(f'La cantidad de documentos encontrados y con el campo weekly_conciliation_transfer_payments puestos en false es: {conteo}')
+        
+
+    except Exception as e:
+        print(f'Error al procesar la consulta: {e}')
+        return None
+    return conteo
+
+while True:
+    print("Elija el proceder que desea ejecutar, teclee el número del consecutivo y presione Enter a continuación")
+    print("")
+    print("1 - Visualizar la cantidad de problemas con la cola de pagos y recargas de terceros")
+    print("2 - Consultar y corregir los documentos que fallaron al actualizarse el estado de cuenta del eCRM")
+    print("3 - Consultar y corregir los documentos que fallaron al cerrar las gestiones de cobro que han sido pagadas")
+    print("4 - Consultar y corregir los documentos que fallaron al descargar el fichero que contiene todas las operaciones de eCRM recibidas por transfermóvil")
+    print("5 - Consultar y corregir los documentos que fallaron al descargar del ftp espejo los ficheros de los bancos para poder ser visualizados y procesados por Facturación")
+    print("6 - Consultar y corregir los documentos que fallaron al actualizar o crear las tablas para la edad de la deuda (Comercial)")
+    print("7 - Consultar y corregir los documentos que fallaron al conciliar los cobros diarios reportados por el banco como aplicados usando Transfermovil")
+    print("8 - Consultar y corregir los documentos que fallaron al conciliar los cobros semanales reportados por el banco como aplicados usando Transfermovil")
+    print("9 - Aplicar todos los procederes de manera consecutiva")
+    print("Para finalizar la aplicación presione la letra (s)")
+    accion = str(input())
+    if accion == "1":
+        problemas_cola(db)
+        pausa = str(input())
+    elif accion == "2":
+        actualiza_estado_de_cuenta_eCRM(db)
+        pausa = str(input())
+    elif accion == "3":
+        cierra_gestiones_de_cobro_eCRM(db)
+        pausa = str(input())
+    elif accion == "4":
+        export_fichero_Transfermovil_eCRM(db)
+        pausa = str(input())
+    elif accion == "5":
+        descarga_fichero_banco_eCRM(db)
+        pausa = str(input())
+    elif accion == "6":
+        crea_tablas_old_debt_comercial_eCRM(db)
+        pausa = str(input())
+    elif accion == "7":
+        concilia_cobros_transfermovil_eCRM(db)
+        pausa = str(input())
+    elif accion == "8":
+        concilia_cobros_semanales_transfermovil_eCRM(db)
+        pausa = str(input())
+    elif accion == "9":
+        problemas_cola(db)
+        actualiza_estado_de_cuenta_eCRM(db)
+        cierra_gestiones_de_cobro_eCRM(db)
+        export_fichero_Transfermovil_eCRM(db)
+        descarga_fichero_banco_eCRM(db)
+        crea_tablas_old_debt_comercial_eCRM(db)
+        concilia_cobros_transfermovil_eCRM(db)
+        concilia_cobros_semanales_transfermovil_eCRM(db)
+        pausa = str(input())
+    elif accion == "s":
+        break
+
+
--- test_procederes.py
+++ test_procederes.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
 from pymongo.collection import Collection
-from procederes import problemas_cola, actualiza_estado_de_cuenta_eCRM  # Reemplaza 'procederes' con el nombre real de tu script
+from procederes import concilia_cobros_transfermovil_eCRM, concilia_cobros_semanales_transfermovil_eCRM, problemas_cola, actualiza_estado_de_cuenta_eCRM, cierra_gestiones_de_cobro_eCRM, export_fichero_Transfermovil_eCRM, descarga_fichero_banco_eCRM, crea_tablas_old_debt_comercial_eCRM  # Reemplaza 'procederes' con el nombre real de tu script
 
 def test_problemas_cola(mocker) -> None:
     """
@@ -48,6 +48,7 @@ def test_problemas_cola_no_transfer(mocker) -> None:
     assert result == 0
     mock_db.get_collection.assert_called_once_with('payment_extern_operation')
     mock_collection.find.assert_called_once_with({"source_type": "TRANSFER"})
+
 
 def test_actualiza_estado_de_cuenta_eCRM(mocker) -> None:
     """
@@ -107,4 +108,370 @@ def test_actualiza_estado_de_cuenta_eCRM_no_match(mocker) -> None:
     mock_collection.update_many.assert_called_once_with(
         {"insert_sgc_account_state": {"$regex": "\"true\"", "$options": "i"}},
         {"$set": {"insert_sgc_account_state": "false"}}
+    )
+
+
+def test_cierra_gestiones_de_cobro_eCRM(mocker) -> None:
+    """
+    Prueba que la función cierra_gestiones_de_cobro_eCRM actualice correctamente los documentos.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 5
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = cierra_gestiones_de_cobro_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 5
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"get_service_to_call_data_from_sgc": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"get_service_to_call_data_from_sgc": "false"}}
+    )
+
+def test_cierra_gestiones_de_cobro_eCRM_no_match(mocker) -> None:
+    """
+    Prueba que la función cierra_gestiones_de_cobro_eCRM devuelva 0 cuando no hay documentos para actualizar.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 0
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = cierra_gestiones_de_cobro_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 0
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"get_service_to_call_data_from_sgc": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"get_service_to_call_data_from_sgc": "false"}}
+    )
+
+
+def test_export_fichero_Transfermovil_eCRM(mocker) -> None:
+    """
+    Prueba que la función export_fichero_Transfermovil_eCRM actualice correctamente los documentos.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 5
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = export_fichero_Transfermovil_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 5
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"export_tranfermovil_operation": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"export_tranfermovil_operation": "false"}}
+    )
+
+def test_export_fichero_Transfermovil_eCRM_no_match(mocker) -> None:
+    """
+    Prueba que la función export_fichero_Transfermovil_eCRM devuelva 0 cuando no hay documentos para actualizar.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 0
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = export_fichero_Transfermovil_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 0
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"export_tranfermovil_operation": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"export_tranfermovil_operation": "false"}}
+    )
+
+
+def test_descarga_fichero_banco_eCRM(mocker) -> None:
+    """
+    Prueba que la función descarga_fichero_banco_eCRM actualice correctamente los documentos.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 5
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = descarga_fichero_banco_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 5
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"insert_in_mongo_ftp_files": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"insert_in_mongo_ftp_files": "false"}}
+    )
+
+def test_descarga_fichero_banco_eCRM_no_match(mocker) -> None:
+    """
+    Prueba que la función descarga_fichero_banco_eCRM devuelva 0 cuando no hay documentos para actualizar.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 0
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = descarga_fichero_banco_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 0
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"insert_in_mongo_ftp_files": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"insert_in_mongo_ftp_files": "false"}}
+    )
+
+
+def test_crea_tablas_old_debt_comercial_eCRM(mocker) -> None:
+    """
+    Prueba que la función descarga_fichero_banco_eCRM actualice correctamente los documentos.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 5
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = crea_tablas_old_debt_comercial_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 5
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"created_old_debt": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"created_old_debt": "false"}}
+    )
+
+def test_crea_tablas_old_debt_comercial_eCRM_no_match(mocker) -> None:
+    """
+    Prueba que la función crea_tablas_old_debt_comercial_eCRM devuelva 0 cuando no hay documentos para actualizar.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 0
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = crea_tablas_old_debt_comercial_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 0
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"created_old_debt": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"created_old_debt": "false"}}
+    )
+
+
+def test_concilia_cobros_transfermovil_eCRM(mocker) -> None:
+    """
+    Prueba que la función concilia_cobros_transfermovil_eCRM actualice correctamente los documentos.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 5
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = concilia_cobros_transfermovil_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 5
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"daily_conciliation_transfer_payments": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"daily_conciliation_transfer_payments": "false"}}
+    )
+
+def test_concilia_cobros_transfermovil_eCRM_no_match(mocker) -> None:
+    """
+    Prueba que la función concilia_cobros_transfermovil_eCRM devuelva 0 cuando no hay documentos para actualizar.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 0
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = concilia_cobros_transfermovil_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 0
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"daily_conciliation_transfer_payments": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"daily_conciliation_transfer_payments": "false"}}
+    )
+
+
+def test_concilia_cobros_semanales_transfermovil_eCRM(mocker) -> None:
+    """
+    Prueba que la función concilia_cobros_semanales_transfermovil actualice correctamente los documentos.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 5
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = concilia_cobros_semanales_transfermovil_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 5
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"weekly_conciliation_transfer_payments": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"weekly_conciliation_transfer_payments": "false"}}
+    )
+
+def test_concilia_cobros_semanales_transfermovil_eCRM_no_match(mocker) -> None:
+    """
+    Prueba que la función concilia_cobros_semanales_transfermovil devuelva 0 cuando no hay documentos para actualizar.
+
+    Args:
+        mocker: El fixture de mock de pytest para crear objetos mock.
+    """
+    # Crear un mock para el resultado de update_many
+    mock_update_result = mocker.MagicMock()
+    mock_update_result.modified_count = 0
+
+    # Crear un mock para la colección 'trace_process'
+    mock_collection = mocker.MagicMock(spec=Collection)
+    mock_collection.update_many.return_value = mock_update_result
+
+    # Crear un mock para la base de datos
+    mock_db = mocker.MagicMock()
+    mock_db.get_collection.return_value = mock_collection
+
+    # Ejecutar la función con el mock de la base de datos
+    result = concilia_cobros_semanales_transfermovil_eCRM(mock_db)
+
+    # Verificar que el resultado sea el esperado
+    assert result == 0
+    mock_db.get_collection.assert_called_once_with('trace_process')
+    mock_collection.update_many.assert_called_once_with(
+        {"weekly_conciliation_transfer_payments": {"$regex": "\"true\"", "$options": "i"}},
+        {"$set": {"weekly_conciliation_transfer_payments": "false"}}
     )
